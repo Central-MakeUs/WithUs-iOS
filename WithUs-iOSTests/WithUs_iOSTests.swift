21204daea2cb2f0a98123ae92c92ae05e2db6fb4
JeeeeSangRyul <wltkdfbf@gmail.com>
2025-12-31 14:02:14 +0900

    Initial Commit

---
+++ WithUs-iOSTests/WithUs_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  WithUs_iOSTests.swift
+//  WithUs-iOSTests
+//
+//  Created by 지상률 on 12/31/25.
+//
+
+import Testing
+@testable import WithUs_iOS
+
+struct WithUs_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
