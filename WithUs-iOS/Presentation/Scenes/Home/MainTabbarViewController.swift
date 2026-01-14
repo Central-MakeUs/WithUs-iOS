2d40e6e7b164c4e7a8eabca49ede5c3cfba71c69
JeeSangRyul <wltkdfbf@gmail.com>
2026-01-14 17:59:29 +0900

    Feat: 카메라 테스트중

--- WithUs-iOS/Presentation/Scenes/Home/MainTabbarViewController.swift
+++ WithUs-iOS/Presentation/Scenes/Home/MainTabbarViewController.swift
@@ -14,8 +14,26 @@ final class HomeViewController: BaseViewController {
     
     weak var coordinator: MainCoordinator?
     
+    private let cameraButton = UIButton().then {
+        $0.setTitle("Camgiera", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .black
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         coordinator?.showInviteModal()
+    }
+    
+    override func setupUI() {
+        view.addSubview(cameraButton)
+    }
+    
+    override func setupConstraints() {
+        cameraButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.center.equalToSuperview()
+        }
     }
 }
